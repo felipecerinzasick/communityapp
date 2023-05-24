1e67a93df789cb40658f7b91416380b93f11d4ba
Felipe Sick <felipesick@mbp-von-felipe.speedport_w_724v_09011603_06_010>
2023-05-24 17:06:07 +0200

    Your commit message

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
