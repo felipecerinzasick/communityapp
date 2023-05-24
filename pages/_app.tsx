d7c9368ed114740d92b92b78e213297fba30f678
Felipe Sick <felipesick@MacBook-Pro-von-Felipe.local>
2023-05-24 19:20:34 +0200

    Your commit message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import awsconfig from '../components/aws-exports'; // Note: Update this import path if required.
 import React, { useState } from 'react';
+import Amplify from "@aws-amplify/core";
 
-let Amplify;
-
-if (typeof window !== 'undefined') {
-  Amplify = require('aws-amplify').default;
-  Amplify.configure(awsconfig);
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
