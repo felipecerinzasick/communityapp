17eaf4385493499bbae3d1b30a20c5c6f8c0c8dc
Felipe Sick <felipesick@MacBook-Pro-von-Felipe.local>
2023-05-24 19:52:47 +0200

    Your commit message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
-import awsconfig from '../aws-exports'; // Note: Update this import path if required.
+import awsconfig from './aws-exports'; // Note: Update this import path if required.
 import Amplify from "@aws-amplify/core";
 
 
